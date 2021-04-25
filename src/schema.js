f7a5bd7ccc1e5ef53a690c1acfc29bc38bec6146
Trygve Lie <trygve.lie@finn.no>
2021-04-25 21:19:57 +0200

    fix: lint love

--- src/schema.js
+++ src/schema.js
@@ -2,9 +2,9 @@ import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const currentDirectory = dirname(fileURLToPath(import.meta.url));
 
-const schema = join(__dirname, '../schema/manifest.schema.json');
+const schema = join(currentDirectory, '../schema/manifest.schema.json');
 const file = fs.readFileSync(schema, 'utf-8');
 
 export default JSON.parse(file);
