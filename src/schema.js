e9a69ab5a47b0158a7c6752b3ce871abe4880487
William Killerud <william.killerud@schibsted.com>
2024-05-13 16:04:43 +0200

    fix: type definitions for schema and validators

--- src/schema.js
+++ src/schema.js
@@ -7,4 +7,53 @@ const currentDirectory = dirname(fileURLToPath(import.meta.url));
 const schema = join(currentDirectory, '../schema/manifest.schema.json');
 const file = fs.readFileSync(schema, 'utf-8');
 
+// The JSON schema is the source of truth.
+// The JSDoc typedefs are for buildtime checks and DX.
+
+/**
+ * @typedef {object} PodletCssSchema
+ * @property {string} value
+ * @property {string} [type="text/css"]
+ * @property {string} [crossorigin]
+ * @property {boolean} [disabled=false]
+ * @property {string} [hreflang]
+ * @property {string} [title]
+ * @property {string} [media]
+ * @property {string} [rel="stylesheet"]
+ * @property {string} [as]
+ * @property {"lazy" | "beforeInteractive" | "afterInteractive"} [strategy]
+ * @property {"content" | "fallback" | "all"} [scope]
+ */
+/**
+ * @typedef {object} PodletJavaScriptSchema
+ * @property {string} value
+ * @property {string} [type]
+ * @property {string} [referrerpolicy]
+ * @property {string} [crossorigin]
+ * @property {string} [integrity]
+ * @property {boolean} [nomodule=false]
+ * @property {boolean} [async=false]
+ * @property {boolean} [defer=false]
+ * @property {"lazy" | "beforeInteractive" | "afterInteractive"} [strategy]
+ * @property {"content" | "fallback" | "all"} [scope]
+ */
+
+/**
+ * @typedef {object} PodletProxySchema
+ * @property {string} target
+ * @property {string} name
+ */
+
+/**
+ * @typedef {object} PodletManifestSchema
+ * @property {string} name
+ * @property {string} version
+ * @property {string} content
+ * @property {string} [fallback=""]
+ * @property {Array<PodletJavaScriptSchema>} [js=[]]
+ * @property {Array<PodletCssSchema>} [css=[]]
+ * @property {Record<string, string> | Array<PodletProxySchema>} [proxy={}]
+ * @property {string} [team=""]
+ */
+
 export default JSON.parse(file);
--- src/validate.js
+++ src/validate.js
@@ -1,43 +1,99 @@
-import Ajv from 'ajv';
+import { Ajv } from 'ajv';
 import formats from 'ajv-formats';
 import schema from './schema.js';
 
-const createValidator = (jsonSchema, ajvOptions = {
-    allowUnionTypes: true,
-}) => {
+/**
+ * @typedef {import("./schema.js").PodletManifestSchema} PodletManifestSchema
+ * @typedef {import("./schema.js").PodletCssSchema} PodletCssSchema
+ * @typedef {import("./schema.js").PodletJavaScriptSchema} PodletJavaScriptSchema
+ * @typedef {import("./schema.js").PodletProxySchema} PodletProxySchema
+ */
+
+/**
+ * @template T
+ * @typedef {(data: T) => { value: T; error: false | Array<import('ajv').ErrorObject>; }} PodiumSchemaValidator
+ */
+
+/**
+ * @template [T=any]
+ * @param {object} jsonSchema
+ * @param {import('ajv').Options} ajvOptions
+ * @returns {PodiumSchemaValidator<T>}
+ */
+const createValidator = (
+    jsonSchema,
+    ajvOptions = {
+        allowUnionTypes: true,
+    },
+) => {
     const ajv = new Ajv(ajvOptions);
+    // @ts-expect-error It is in fact callable
     formats(ajv); // Needed to support "uri"
+
     const validate = ajv.compile({
         $schema: 'http://json-schema.org/draft-07/schema#',
         ...jsonSchema,
     });
 
-    return data => {
+    /**
+     * @type {PodiumSchemaValidator<T>}
+     */
+    const validator = (data) => {
         const valid = validate(data);
         return { value: data, error: !valid && validate.errors };
     };
+    return validator;
 };
 
-const withTrimmer = validator => data =>
+/**
+ * @template [T=any]
+ * @param {PodiumSchemaValidator<T>} validator
+ * @returns {PodiumSchemaValidator<T>}
+ */
+const withTrimmer = (validator) => (data) =>
     validator(
         Object.prototype.toString.call(data) === '[object String]'
-            ? data.trim()
+            ? /** @type {T} */ (/** @type {string} */ (data).trim())
             : data,
     );
 
+/** @type {PodiumSchemaValidator<PodletManifestSchema>} */
 export const manifest = createValidator(schema, {
     removeAdditional: true,
     allowUnionTypes: true,
     useDefaults: true,
 });
 
-export const uriStrict = createValidator({type: 'string', format: 'uri'});
+/** @type {PodiumSchemaValidator<string>} */
+export const uriStrict = createValidator({ type: 'string', format: 'uri' });
+
+/** @type {PodiumSchemaValidator<string>} */
 export const fallback = createValidator(schema.properties.fallback);
+
+/** @type {PodiumSchemaValidator<string>} */
 export const version = withTrimmer(createValidator(schema.properties.version));
+
+/** @type {PodiumSchemaValidator<string>} */
 export const content = createValidator(schema.properties.content);
+
+/** @type {PodiumSchemaValidator<Record<string, string> | Array<PodletProxySchema>>} */
 export const proxy = createValidator(schema.properties.proxy);
+
+/** @type {PodiumSchemaValidator<string>} */
 export const team = createValidator(schema.properties.team);
+
+/** @type {PodiumSchemaValidator<string>} */
 export const name = withTrimmer(createValidator(schema.properties.name));
-export const uri = createValidator({type: 'string', format: 'uri-reference', 'minLength': 1});
+
+/** @type {PodiumSchemaValidator<string>} */
+export const uri = createValidator({
+    type: 'string',
+    format: 'uri-reference',
+    minLength: 1,
+});
+
+/** @type {PodiumSchemaValidator<Array<PodletCssSchema>>} */
 export const css = createValidator(schema.properties.css);
+
+/** @type {PodiumSchemaValidator<Array<PodletJavaScriptSchema>>} */
 export const js = createValidator(schema.properties.js);
